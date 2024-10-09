01ce5743bd77357cc97427aa99d1720041fb6985
Kowaio <guillaume.siorak@gmail.com>
2024-10-09 10:21:01 +0200

    meta + email update

--- src/mail/contact_me.php
+++ src/mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'kowaio@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'contact.reveanime@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Formulaire de contact - Site internet :  $name";
 $email_body = "Vous avez reçu une nouveau message depuis votre formulaire de contact.\n\n"."Voici les détails :\n\Nom: $name\n\nE-mail: $email_address\n\nTél.: $phone\n\nMessage:\n$message";
-$headers = "From: kowaio@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: contact.reveanime@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
